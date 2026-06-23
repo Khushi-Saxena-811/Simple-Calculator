566eb349cc0aa02b6b0711e7abaf4dc9c7545941
Khushi Saxena <saxenakhushi891@gmail.com>
2026-06-23 13:31:12 +0530

    added function code in main branch

---
+++ Python_project/project.py
@@ -0,0 +1,24 @@
+#Python_projectclear
+def add():
+    a=int(input("\nPlease enter the first number :"))
+    b=int(input("Please enter the second number :"))
+    c=a+b
+    print("\nAddition of the numbers :",c)
+
+def sub():
+    a=int(input("\nPlease enter the first number :"))
+    b=int(input("Please enter the second number :"))
+    c=a-b
+    print("\nSubtraction of the numbers :",c)
+
+def mul():
+    a=int(input("\nPlease enter the first number :"))
+    b=int(input("Please enter the second number :"))
+    c=a*b
+    print("\nMultiplication of the numbers :",c)
+
+def div():
+    a=int(input("\nPlease enter the first number :"))
+    b=int(input("Please enter the second number :"))
+    c=a//b
+    print("\nDivision of the numbers :",c)
