4ba1caa24e2922b2592bb0126cc5a76a3a0f2515
Khushi Saxena <saxenakhushi891@gmail.com>
2026-06-23 14:43:41 +0530

    added menu successfully

--- Python_project/project.py
+++ Python_project/project.py
@@ -22,3 +22,43 @@ def div():
     b=int(input("Please enter the second number :"))
     c=a//b
     print("\nDivision of the numbers :",c)
+
+def menu():
+    while(True):
+        print("======================================================")
+        print("............Welcome to the calculator............")
+        print("======================================================\n")
+        print("Type 1 for the addition of the two numbers :")
+        print("Type 2 for the subtraction of the two numbers :")
+        print("Type 3 for the multiplication of the two numbers :")
+        print("Type 4 for the division of the two numbers :")
+        k=int(input())
+        print("\n----------------------------------------------------")
+        if(k==1):
+            add()
+        elif(k==2):
+            sub()
+        elif(k==3):
+            mul()
+        elif(k==4):
+            div()
+        else:
+            print("\n----------------------------------------------------")
+            print("You've entered invalid input !")
+            print("Please TRY AGAIN !")
+            exit()
+        print("\n----------------------------------------------------")
+        print("\nType 'y' for reusing the calculator :")
+        print("Type 'n' for ending the program :")
+        p=input()
+        if(p=='y' or p=='Y'):
+            continue
+        else:
+            print("\n----------------------------------------------------")
+            print("\nThank you so much for using our calculator !")
+            print("Take care :)")
+
+            print("\nProgram ended savely ....")
+            print("\n-------------------------X---------------------------")
+            exit()
+menu()
